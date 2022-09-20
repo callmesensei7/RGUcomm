222a42c22cf8ced89f9c90286a6df6fa3ced9acd
callmesensei7 <c.andy-iwebelua@rgu.ac.uk>
2022-09-20 21:27:17 +0100

    just now 20/09

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -172,7 +172,7 @@ class Post {
 									<img src='$profile_pic' width='50'>
 								</div>
 
-								<div class='posted_by' style='color:#ACACAC;'>
+								<div class='posted_by' style='color:#7b7286;'>
 									<a href='$added_by'> $first_name $last_name </a> $user_to &nbsp;&nbsp;&nbsp;&nbsp;$time_message
 								</div>
 								<div id='post_body'>
